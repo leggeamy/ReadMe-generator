7b48869ff96b7de42287eb5c778ffaca03b468a8
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-01 22:42:37 -0500

    updates to generateMarkdowwn, testing

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,12 +1,34 @@
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return ''
+}
+
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n*[License](#license)\n`
+    )
+  }
+  return ''
+}
+
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License
+
+      This project is licensed under the ${license} license.`
+    )
+  }
+  return ''
+}
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
-
-`;
-}
-
-module.exports = generateMarkdown;
 
 ## Description
 
@@ -26,4 +48,36 @@ ${renderLicenseLink(data.license)}
 
 ## Installation
 
+To install necessary dependencies, run the following command:
+
 \`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+To run tests, enter the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions about this repository, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+
+`;
+}
+
+
+module.exports = generateMarkdown;

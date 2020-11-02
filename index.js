452315b9763f68e07c7d802656027786990b2eb1
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-01 20:37:22 -0500

    attempting to reinstall inquirer, restructuring files, initial question testing

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const path = require('path');
-const generateMarkdown = require("../Utils/generateMarkdown");
+// const path = require("path");
+// const generateMarkdown = require("../Utils/generateMarkdown");
 
+console.log(inquirer)
 
 // array of questions for user
- const questions = [
+const questions = [
     {
         type: 'input',
         name: 'name',
         message: 'What is your name?'
-      },
-console.log(answers),
+      }
+]
+.then(answers => console.log(answers));
+;
 
 // function to write README file
-// function writeToFile(fileName, data) {
-// }
+function writeToFile(fileName, data) {
+}
 
 // function to initialize program
-// function init() {
+function init() {
 
-// }
+}
 
 // function call to initialize program
-// init();
+init();
